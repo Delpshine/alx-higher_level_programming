eeed05b903c0e092bea2f04cb81b294f00aa24a7
philadelphia olawale <philadelphiasamuel@gmail.com>
2023-09-11 21:53:46 -0700

    11-delete_at.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    "To replace an element in a copied list at a specified index."
+    """To replace an element in a copied list at a specified index."""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
